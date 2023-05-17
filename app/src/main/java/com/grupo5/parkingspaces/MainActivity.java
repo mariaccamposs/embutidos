43e78e85bc4246e128658f8365c224d424a959d9
André Cerqueira <acerqueira021@gmail.com>
2023-05-17 11:40:34 +0100

    Create grid

--- app/src/main/java/com/grupo5/parkingspaces/MainActivity.java
+++ app/src/main/java/com/grupo5/parkingspaces/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +19,10 @@ import com.grupo5.parkingspaces.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +42,59 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+
+        GridLayout gridLayout = new GridLayout(this);
+        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams();
+        layoutParams.height = GridLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.width = GridLayout.LayoutParams.MATCH_PARENT;
+        layoutParams.setMargins(10, 10, 10, 10);
+        gridLayout.setLayoutParams(layoutParams);
+
+        gridLayout.setColumnCount(3);
+        gridLayout.setRowCount(5);
+
+        ViewGroup root =  (ViewGroup) findViewById(R.id.mainpage);
+
+        int[] arr = {3, 4, 3, 2};
+        int r = 0, c = 0;
+        for (int i=0; i<4; i++) {
+            for(int j=0; j<arr[i]; j++) {
+                CardView cardView  =  createChild();
+                if( c == 3 ) {
+                    c = 0;
+                    r++;
+                }
+
+                Button button  = new Button(this);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Snackbar.make(v , "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();                    }
+                });
+//                ImageView imageView = new ImageView(this);
+//                imageView.setImageResource(R.mipmap.ic_launcher);
+//                imageView.setLayoutParams(new ViewGroup.LayoutParams(150, 150));
+
+                GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
+                GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
+
+//                if(r == 0 && c == 0) {
+//                    colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1);
+//                    rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 2);
+//                }
+
+                GridLayout.LayoutParams gridParam = new GridLayout.LayoutParams(
+                        rowSpan, colSpan
+                );
+                cardView.addView(button);
+                gridLayout.addView(cardView, gridParam);
+                r++; c++;
+            }
+
+        }
+
+        root.addView(gridLayout);
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +102,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private CardView createChild() {
+            CardView cardView = new CardView(this);
+            cardView.setCardElevation(2);
+            cardView.setUseCompatPadding(true);
+            ViewGroup.LayoutParams cvLayoutParams = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            cardView.setLayoutParams(cvLayoutParams);
+            return cardView;
     }
 
     @Override
